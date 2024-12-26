33abb9a18922b962031b7661b66de428d6631d20
alysonrafael <alysonsrafael840@gmail.com>
2024-12-26 12:27:25 -0300

    python

---
+++ test1/teste.py
@@ -0,0 +1,21 @@
+# print("Ola,mundo")
+# media = 0
+# n1=n2=n3=n4=0.0
+# nome, idade = 'Alyson', 18
+# estado = False #valor logíco
+# # função type()
+# print(type(media))
+# print(type(n1))
+# print(type(nome))
+# print(type(estado))
+# print(type(1+2j))
+
+#função isinstancce() 
+a =10
+b = 'sol'
+print(isinstance(a, (bool,int))) # PEGA A VARIAVEL A E VE SE O TIPO QUE VOCE PASSOU E O MESMO DA VARIAVEL
+
+a = 40
+c = 3
+r = a * c
+print(r)
